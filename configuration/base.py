d85a9c511218bfce1aa98329e4601dba905bcd69
Тимофей Смирнов <timothyxp@mail.ru>
2020-01-24 14:27:57 +0300

    fox boosting pipeline

--- configuration/base.py
+++ configuration/base.py
@@ -1,11 +1,12 @@
 from pipeline.config_base import ConfigBase
-from catboost import CatBoostClassifier
+from catboost import CatBoostRegressor
 from pipeline.feature_extractors.base import FeatureExtractorCombiner, FeatureExtractorTimeStackWrapper
 from pipeline.feature_extractors.time import FeatureExtractorTime
 from pipeline.feature_extractors.radius import FeatureExtractorRadius
 from pipeline.data.default import DataBuilderDefault
 from pipeline.feature_extractors.base import FeatureExtractorCombiner
 from pipeline.models.base import ModelBoosting
+from sklearn.ensemble import RandomForestRegressor
 
 
 class Config(ConfigBase):
@@ -14,10 +15,17 @@ class Config(ConfigBase):
             FeatureExtractorCombiner([
                 FeatureExtractorRadius(),
                 FeatureExtractorTime()
-            ])
+            ]),
+            num_ex=5
         )
 
-        model = ModelBoosting(self)
+        model = ModelBoosting(self, lambda: CatBoostRegressor(
+            learning_rate=0.1,
+            max_depth=5,
+            iterations=200,
+            thread_count=8,
+            loss_function='MAE'
+        ))
 
         super().__init__(
             experiment_name="main",
